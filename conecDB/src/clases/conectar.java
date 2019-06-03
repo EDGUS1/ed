68b93a02bea609374d9cd0d21a711ccd41175664
EduardoNav <49346398+EDGUS1@users.noreply.github.com>
2019-06-02 20:41:17 -0500

    Add files via upload

---
+++ conecDB/src/clases/conectar.java
@@ -0,0 +1,35 @@
+
+package clases;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conectar {
+    private static Connection conn;
+
+    public conectar() {
+        conn = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/dbjava","root","eduardo");
+            if(conn != null){
+                System.out.println("conectado");
+            }
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("Error al conectar"+e);
+        }
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+    
+    public void desconectar(){
+        conn = null;
+        if(conn == null){
+            System.out.println("conexion terminada");
+        }
+    }
+    
+}
